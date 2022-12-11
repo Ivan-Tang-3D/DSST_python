4655163c455f60092f4c15b1a5e7f27e71d97ea2
Yihao Wang <18307110360@fudan.edu.cn>
2022-12-11 14:56:30 +0800

    1. modify README.md 2. add exception handling

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# DSST_Python
 
 ## Requirements
 - Python 3.7
@@ -5,4 +6,22 @@
 - Numba (needed if you want to use the hog feature)
 - OpenCV (ensure that you can import cv2 in python) need opencv-contrib-python and the version of opencv-python is 3.4.5.36
 
-#you need to run the run.py using compressed video
+## How to run tracking
+
+track with a given video:
+
+```shell
+python run.py --mode video
+```
+
+track with a stream from a camera:
+
+```shell
+python run.py --mode builtin_stream
+```
+
+or
+
+```shell
+python run.py --mode outer_stream
+```
--- run.py
+++ run.py
@@ -9,31 +9,32 @@ def tracker(cam, frame, bbox):
     tracker = KCFTracker(True, True, True)  # (hog, fixed_Window, multi_scale)
     tracker.init(bbox, frame)
 
-    while True:
-        ok, frame = cam.read()
+    try:
+        while True:
+            ok, frame = cam.read()
 
-        timer = cv2.getTickCount()
-        bbox = tracker.update(frame)
-        bbox = list(map(int, bbox))
-        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+            timer = cv2.getTickCount()
+            bbox = tracker.update(frame)
+            bbox = list(map(int, bbox))
+            fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
 
-        # Tracking success
-        p1 = (int(bbox[0]), int(bbox[1]))
-        p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-        cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
 
-        # Put FPS
-        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+            # Put FPS
+            cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
 
-        cv2.imshow("Tracking", frame)
+            cv2.imshow("Tracking", frame)
 
-        # Exit if ESC pressed
-        k = cv2.waitKey(1) & 0xff
-        if k == 27:
-            break
-
-    cam.release()
-    cv2.destroyAllWindows()
+            # Exit if ESC pressed
+            k = cv2.waitKey(1) & 0xff
+            if k == 27:
+                break
+    finally:
+        cv2.destroyAllWindows()
+        cam.release()
 
 
 def run(args):
@@ -44,11 +45,14 @@ def run(args):
     elif args.mode == 'outer_stream':
         video = cv2.VideoCapture(1)  # use outer camera to collect video stream
     fps = video.get(cv2.CAP_PROP_FPS)
-    print(fps)
+    print(f"fps:\t{fps}")
     size = (int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    print(size)  # image size
-    ok, frame = video.read()
-    bbox = cv2.selectROI('Select ROI', frame, False)
+    print(f"size:\t{size}")  # image size
+    try:
+        ok, frame = video.read()
+        bbox = cv2.selectROI('Select ROI', frame, False)
+    finally:
+        cv2.destroyAllWindows()
     if min(bbox) == 0:
         exit(0)
     tracker(video, frame, bbox)
@@ -56,9 +60,8 @@ def run(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--mode', type=str,
-                        choices=['video', 'builtin_stream', 'outer_stream'], required=True, help="")
-
+    parser.add_argument('-m', '--mode', type=str, required=True,
+                        choices=['video', 'builtin_stream', 'outer_stream'], help="Select a tracking mode.")
     args = parser.parse_args()
 
     run(args)
